064ab48554e6cdd1568f41759be866d02b94e41c
shreyasri258 <kertishreya2508@gmail.com>
2023-01-23 20:38:17 +0530

    custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
+function ExpenseItem(props) {
+     
   return (
     <div className='expense-item'>
-      <div>March 16 2023 </div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$560.54</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
