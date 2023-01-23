c6e1028efcc7f3e1adaeec4a93ddfe8785c48825
shreyasri258 <kertishreya2508@gmail.com>
2023-01-23 20:58:47 +0530

    splitting components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
+import CalenderItem from './CalenderItem';
 import './ExpenseItem.css';
+
+
 function ExpenseItem(props) {
-     
-  return (
-    <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+    return(
+   <div className='expense-item'>
+
+      <CalenderItem date={props.date}></CalenderItem>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+   </div>
   );
 }
 
