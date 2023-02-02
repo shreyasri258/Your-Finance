5a4c87d816735ec7eedcb108996bd038ac8e4992
shreyasri258 <kertishreya2508@gmail.com>
2023-02-02 20:27:30 +0530

    handling multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,31 +1,51 @@
-import React from "react";
-import './ExpenseForm.css'
-const ExpenseForm=()=>{
-return <form>
-    <div className="new-expense__controls">
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+const ExpenseForm = () => {
+  const [Title, SetTitle] = useState("");
+  const [Amount, SetAmount] = useState("");
+  const [Date, SetDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    SetTitle(event.target.value);
+  };
+  const AmountChangeHandler = (event) => {
+    SetAmount(event.target.value);
+  };
+  const DateChangeHandler = (event) => {
+    SetDate(event.target.value);
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
         <div className="new-expense__controls">
-            <label>
-                Title
-            </label>
-            <input type="text"></input>
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__controls">
-            <label>
-                Amount
-            </label>
-            <input type="number" min="0.01" step="0.01"></input>
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={AmountChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__controls">
-            <label>
-                Date
-            </label>
-            <input type="date" min="2018-01-01" max="2025-12-31"></input>
+          <label>Date</label>
+          <input
+            type="date"
+            min="2018-01-01"
+            max="2025-12-31"
+            onChange={DateChangeHandler}
+          ></input>
         </div>
-    </div>
-    <div className="new-expense__actions">
-<button type="submit">Add Expense</button>
-    </div>
-</form>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
